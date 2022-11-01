f3fb0fd90e54063814a106aa5c292e8d0a166ffe
Cosmin Iancu <ianstecos@gmail.com>
2022-11-01 21:44:16 +0000

    assignment exercise: part 2

--- p14/main.py
+++ p14/main.py
@@ -1,15 +1,24 @@
 # https://www.youtube.com/watch?v=ZDa-Z5JzLYM
 
 class Employee:
+    num_of_emps = 0
+    raise_amt = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@email.com'
+        self.email = first + '.' + last + '@company.com'
         self.pay = pay
+
+        Employee.num_of_emps += 1
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
+      self.pay = int(self.pay * self.raise_amt)
+
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
+
+print(Employee.num_of_emps)
